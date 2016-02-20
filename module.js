6a8637b5ec94b3d790e618b65a6eceb8911a6de6
MatteoVH <matteovh@ucla.edu>
2016-02-19 21:34:39 -0800

    implement createFullBinaryTree
    This is an auxiliary function that allows the creation of a
    binary search tree for a specified number of levels and with the
    end level filled with each individual character of the string
    passed as the second argument. It is useful to create binary
    search trees for the base 64 encoding so that the correct
    character to represent a 6-bit stream can be found in log(n)
    time.

--- module.js
+++ module.js
@@ -36,6 +36,36 @@ const gem = {
 			case 'f':
 				return [1, 1, 1, 1];
 		}
+	},
+
+	createFullBinaryTree: function(totalLevels, leavesContent) {
+		//if we don't receive a string or array long enough to have full bottom level, we return null
+		if (leavesContent.length !== Math.pow(2, totalLevels))
+			return null;
+
+		if (totalLevels === 0)
+			return null;
+
+		let binaryTreeRoot = new Array(2);
+
+		let leavesContentIndex = 0;
+			
+		function fillTree(node, level) {
+			
+			if (level === totalLevels) {
+				node[0] = leavesContent[leavesContentIndex];
+				leavesContentIndex++;
+				node[1] = leavesContent[leavesContentIndex];
+				leavesContentIndex++;
+			} else {
+				node[0] = fillTree(new Array(2), level + 1);
+				node[1] = fillTree(new Array(2), level + 1);
+			}
+
+			return node;
+		}
+
+		return fillTree(binaryTreeRoot, 1);
 	}
 }
 
